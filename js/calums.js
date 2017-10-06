a2fdc5560e5371d8687083f3104ba25c09470e1e
sselv4 <suganthan53@gmail.com>
2017-10-06 19:27:18 -0400

    Scroll to top icon added and Bottom Scroll Revision

--- js/calums.js
+++ js/calums.js
@@ -260,6 +260,39 @@ $(document).ready(function(){
 });
 
 
+/*********************scrollTop***************/
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$()
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+$(window).scroll(function() {
+    if ($(this).scrollTop()>6400)
+    {
+        $('.scroll-down').fadeOut();
+    }
+    else
+    {
+        $('.scroll-down').fadeIn();
+    }
+
+});
+$(document).ready(function() {
+    $("#down-arrow").click(function(event){
+        $('html, body').animate({scrollTop: '+=750px'}, 800);
+    });
+});
+
+
+
 
 
 /* =================================
